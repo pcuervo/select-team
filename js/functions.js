83ddcb61a4664ad4d6271c702aa47840ae6edf74
piers55 <apiers55@gmail.com>
2014-10-21 19:50:31 -0500

    first commit

---
+++ js/functions.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+  /*
+  ** Para el video cuando se cierra el modal
+  */
+  $('.modal').each(function(){
+    var src = $(this).find('iframe').attr('src');
+    $(this).on('click', function(){
+      $(this).find('iframe').attr('src', '');
+      $(this).find('iframe').attr('src', src);
+    });
+  });
+});
+
+
+
+
+
+function correIsotope(contenedor, item, layoutMode){
+  var $container = $(contenedor);
+  $container.isotope({
+    itemSelector: item,
+    layoutMode: layoutMode
+  });
+}
+
+
+
+
+function filtrarIsotopeDefault(contenedor, filterDefault){
+  var $container = $(contenedor);
+  $container.isotope({ filter: filterDefault });
+}
+
+
+
+
+function filtrarIsotope(este, contenedor, elemento){
+  var filterValue = este.data('filter');
+  var $container = $(contenedor);
+  $container.isotope({ filter: filterValue });
+  $(elemento).removeClass('active');
+  este.addClass('active');
+}
+
+
+
+
+
+function reorder(este, contenedor){
+  var sport = $('#sportAll').attr('data-active');
+  var gender = $('#genderAll').attr('data-active');
+  if (typeof sport === 'undefined') {
+      sport = "";
+  }
+  if (typeof gender === 'undefined') {
+      gender = "";
+  }
+  var filterString = sport+gender;
+  if(filterString=="**"){
+      filterString = "*"; 
+  }
+  este.parents('.button-group').find('.active').removeClass('active');
+  este.addClass('active');
+  var $container = $(contenedor);
+  $container.isotope({
+      filter: filterString,
+      animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false,
+      }
+  });         
+}
+
+
+
