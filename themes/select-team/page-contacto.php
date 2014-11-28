19d88f24d21f45c152b7ecc28e4183a0fa143c0a
raul <raul@zamacona.me>
2014-11-28 16:34:14 -0600

    más traducciones por ahí.

--- themes/select-team/page-contacto.php
+++ themes/select-team/page-contacto.php
@@ -19,33 +19,60 @@
             <!-- <p><a href="#" target="_blank"><i class="fa fa-twitter"></i> @selectteam</a></p> -->
             <hr>
             <div>
-                <p><b>Luis Mendoza (President)</b></p>
+                <?php if (qtrans_getLanguage() == 'es'){ ?>
+                    <p><b>Luis Mendoza (Presidente)</b></p>
+                <?php } else { ?>
+                    <p><b>Luis Mendoza (President)</b></p>
+                <?php } ?>
                 <p><i class="fa fa-phone"></i> 7773067947</p>
                 <p><i class="fa fa-envelope"></i> <a href="mailto:luis.mendoza@selectteam.com">luis.mendoza@selectteam.com</a></p>
             </div>
             <hr>
             <div>
-                <p><b>Nair Tolomeo (Vicepresident)</b></p>
-                <!-- <p><i class="fa fa-phone"></i> </p> -->
+                <?php if (qtrans_getLanguage() == 'es'){ ?>
+                    <p><b>Nair Tolomeo (Vicepresidente)</b></p>
+                <?php } else { ?>
+                    <p><b>Nair Tolomeo (Vicepresident)</b></p>
+                <?php } ?>
                 <p><i class="fa fa-envelope"></i> <a href="mailto:info@selectteam.com">info@selectteam.com</a></p>
             </div>
         </div>
         <div class="col-xs-12 col-sm-6 margin-bottom">
-            <h3 class="red">Leave a comment</h3>
+            <?php if (qtrans_getLanguage() == 'es'){ ?>
+                <h3 class="red">Deja un comentario</h3>
+            <?php } else { ?>
+                <h3 class="red">Leave a comment</h3>
+            <?php } ?>
             <form role="form">
                 <div class="form-group">
-                    <label for="name">Name</label>
+                    <?php if (qtrans_getLanguage() == 'es'){ ?>
+                        <label for="name">Nombre</label>
+                    <?php } else { ?>
+                        <label for="name">Name</label>
+                    <?php } ?>
                     <input type="text" class="form-control" id="name" placeholder="Name">
                 </div>
                 <div class="form-group">
-                    <label for="mail">Email address</label>
+                    <?php if (qtrans_getLanguage() == 'es'){ ?>
+                        <label for="mail">E-mail</label>
+                    <?php } else { ?>
+                        <label for="mail">E-mail address</label>
+                    <?php } ?>
                     <input type="email" class="form-control" id="mail" placeholder="Enter email">
                 </div>
                 <div class="form-group">
-                    <label for="mail">Write your comment</label>
+                    <?php if (qtrans_getLanguage() == 'es'){ ?>
+                        <label for="mail">Escribe tu comentario</label>
+                    <?php } else { ?>
+                        <label for="mail">Write your comment</label>
+                    <?php } ?>
                     <textarea class="form-control" rows="3"></textarea>
                 </div>
-                <button type="submit" class="btn btn-primary right">Submit</button>
+                <?php if (qtrans_getLanguage() == 'es'){ ?>
+                    <button type="submit" class="btn btn-primary right">Enviar</button>
+                <?php } else { ?>
+                    <button type="submit" class="btn btn-primary right">Submit</button>
+                <?php } ?>
             </form>
         </div>
     </div>
