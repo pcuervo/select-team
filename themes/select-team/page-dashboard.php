f381f3011fdb0de07f8ec205c259d96cb1825202
zurol <zurol@pcuervo.com>
2014-11-28 20:42:44 -0600

    select Incompleto

--- themes/select-team/page-dashboard.php
+++ themes/select-team/page-dashboard.php
@@ -194,25 +194,37 @@ include_once('send-prospects.php');
                                                         <option value="86-88">86-88</option>
                                                         <option value="89-90">89-90</option>
                                                     <?php break;
-                                                case '77-79': ?>
+                                                case '86-88': ?>
                                                         <option value="-66" >Under 66</option>
                                                         <option value="66-67">66-67</option>
                                                         <option value="68-70">68-70</option>
                                                         <option value="71-73">71-73</option>
                                                         <option value="74-76">74-76</option>
-                                                        <option value="77-79" selected>77-79</option>
+                                                        <option value="77-79" >77-79</option>
+                                                        <option value="80-82">80-82</option>
+                                                        <option value="83-85">83-85</option>
+                                                        <option value="86-88" selected>86-88</option>
+                                                        <option value="89-90">89-90</option>
+                                                    <?php break;
+                                                case '89-90': ?>
+                                                        <option value="-66" >Under 66</option>
+                                                        <option value="66-67">66-67</option>
+                                                        <option value="68-70">68-70</option>
+                                                        <option value="71-73">71-73</option>
+                                                        <option value="74-76">74-76</option>
+                                                        <option value="77-79" >77-79</option>
                                                         <option value="80-82">80-82</option>
                                                         <option value="83-85">83-85</option>
                                                         <option value="86-88">86-88</option>
-                                                        <option value="89-90">89-90</option>
+                                                        <option value="89-90" selected>89-90</option>
                                                     <?php break;
-                                                case '77-79': ?>
+                                                default: ?>
                                                         <option value="-66" >Under 66</option>
                                                         <option value="66-67">66-67</option>
                                                         <option value="68-70">68-70</option>
                                                         <option value="71-73">71-73</option>
                                                         <option value="74-76">74-76</option>
-                                                        <option value="77-79" selected>77-79</option>
+                                                        <option value="77-79">77-79</option>
                                                         <option value="80-82">80-82</option>
                                                         <option value="83-85">83-85</option>
                                                         <option value="86-88">86-88</option>
