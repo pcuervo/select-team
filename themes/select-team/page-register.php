f97da4504bb8e990d59e4cf3ea174b782fe5d358
zurol <zurol@pcuervo.com>
2014-12-01 16:53:56 -0600

    gender

--- themes/select-team/page-register.php
+++ themes/select-team/page-register.php
@@ -77,7 +77,7 @@
                             <?php } else { ?>
                                 <label for="date_of_birth">Date of birth</label>
                             <?php } ?>
-                            <input class="[ form-control ] [ .j-datepicker ]" id="datepicker-date-of-birth" name="date_of_birth"  value="<?php echo $_GET['q3']; ?>"/>
+                            <input type="date" class="[ form-control ] [ .j-datepicker ]" id="datepicker-date-of-birth" name="date_of_birth"  value="<?php echo $_GET['q3']; ?>"/>
                         </div>
                         <div class="[ form-group ] [ col-xs-12 ]">
                             <?php if (qtrans_getLanguage() == 'es'){ ?>
