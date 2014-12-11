8f8cd784d4028aa0ffc4454aee968c8d954470c0
piers55 <apiers55@gmail.com>
2014-12-10 20:33:33 -0600

    correcciones varias

--- themes/select-team/page-dashboard.php
+++ themes/select-team/page-dashboard.php
@@ -48,7 +48,7 @@
             <div class="[ container-fluid ]" id="page-content">
                 <a href="#menu-toggle" id="menu-toggle" class="[ hidden-md hidden-lg ]"><i class="[ fa fa-bars fa-2x ]"></i></a>
                 <div class="[ row ] [ dashboard-profile ] [ margin-bottom ]"  id="upload_picture">
-                    <div class="[ col-xs-12 col-sm-7 ] [ center block ]">
+                    <div class="[ col-xs-12 col-sm-7 ]">
                         <?php if (qtrans_getLanguage() == 'es'){ ?>
                             <h3>Foto de perfil</h3>
                         <?php } else { ?>
@@ -107,7 +107,7 @@
                     </div>
                 </div>
                 <div class="[ row ] [ dashboard-profile ] [ margin-bottom ]" id="profile">
-                    <div class="[ col-xs-12 col-sm-7 center block ]">
+                    <div class="[ col-xs-12 col-sm-7 ]">
                         
                         <?php if (qtrans_getLanguage() == 'es'){ ?>
                             <h3>Perfil</h3>
@@ -362,7 +362,7 @@
                     </div>
                 </div>
                 <div class="[ row ] [ dashboard-profile ] [ margin-bottom ]" id="curriculum">
-                    <div class="[ col-xs-12 col-sm-7 ] [ center block ]">
+                    <div class="[ col-xs-12 col-sm-7 ]">
                         <h3>Curriculum</h3>
                         <?php if (qtrans_getLanguage() == 'es'){ ?>
                             <p class="help-block">Esta section no será visible en tu perfil público.</p>
@@ -528,7 +528,7 @@
                     </div>
                 </div>
                 <div class="[ row ] [ dashboard-profile ] [ margin-bottom ]"  id="messages">
-                    <div class="[ col-xs-12 col-sm-7 ] [ center block ]">
+                    <div class="[ col-xs-12 col-sm-7 ]">
                         <?php if (qtrans_getLanguage() == 'es'){ ?>
                             <h3>Mensajes</h3>
                             <p class="help-block">Envía un mensaje a uno de nuestros agentes.</p>
