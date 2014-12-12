e77afea96a756c73103b62a21e14dc3eb3b4c5ef
piers55 <apiers55@gmail.com>
2014-12-12 11:20:31 -0600

    tamaño de modal para video de prospecto

--- themes/select-team/single-prospecto.php
+++ themes/select-team/single-prospecto.php
@@ -195,7 +195,7 @@
 
   <!-- STUDENT VIDEO -->
 	<div class="modal fade" id="student-video" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	  	<div class="modal-dialog">
+	  	<div class="modal-dialog modal-lg">
 			<div class="modal-content">
 		  		<div class="modal-header clearfix">
 					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
